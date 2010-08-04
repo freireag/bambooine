19ae6f8b7184411acc341ba7d44dd0d80d92572b
Thiago Freire <freireag@gmail.com>
2010-08-04 07:29:27 -0300

    Removing everything

--- app/helpers/application_helper.rb
+++
@@ -1,9 +0,0 @@
-# Methods added to this helper will be available to all templates in the application.
-module ApplicationHelper
-  def tag_index
-    items = Post.tag_counts.map do |tag|
-      tag_item = content_tag(:li, link_to(tag.name, :controller => 'posts', :action => 'tag', :id => tag.name))
-    end
-    content_tag(:ul, items, :class => 'tags')
-  end
-end
--- vendor/rails/railties/helpers/performance_test_helper.rb
+++
@@ -1,6 +0,0 @@
-require 'test_helper'
-require 'action_controller/performance_test'
-
-ActionController::Base.perform_caching = true
-ActiveSupport::Dependencies.mechanism = :require
-Rails.logger.level = ActiveSupport::BufferedLogger::INFO
